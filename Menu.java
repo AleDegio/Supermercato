6f634ca30f2a0568cd09ced914d3c96620735bc0
AleDegio <50446554+AleDegio@users.noreply.github.com>
2020-09-23 08:18:14 +0200

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,9 +1,33 @@
+import javax.swing.*;
+
 public class Menu {
+    JMenu m, mProd;
+    JMenuItem ricValRep, ricTellResp, add, ricerca1, ricerca2, delate, update; //nodi del menu
+    Menu(){
+        JMenuBar mb = new JMenuBar(); //barra del menu
+        m = new JMenu("Menu Grande magazzino"); //menu principale
+        mProd = new JMenu("Menu Prodotto"); //menu prodotto
+        mb.add(m);
 
-    public static void main(String[] argv) {
-        System.out.println("test");
+        ricValRep = new JMenuItem("Ricerca reparto");
+        ricTellResp = new JMenuItem("Ricerca num. Telefono responsabile");
+        m.add(ricValRep);
+        m.add(ricTellResp);
+
+        m.add(mProd);
+        add = new JMenuItem("Aggiungi");
+        ricerca1 = new JMenuItem("Ricerca tramite il NOME");
+        ricerca2 = new JMenuItem("Ricerca tramite il CODICE");
+        delate = new JMenuItem("Cancella");
+        update = new JMenuItem("Aggiorna");
+
+        mProd.add(add);
+        mProd.add(ricerca1);
+        mProd.add(ricerca2);
+        mProd.add(delate);
+        mProd.add(update);
     }
-    
-    
-
+    public static void main(String[] argv) {
+        new Menu();
+    }
 }
